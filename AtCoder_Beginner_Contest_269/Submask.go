cb1b5a957fe13f0947c7753fca1bd5236971f3d2
RintaroNagano <rintaro.nagano@gmail.com>
2022-09-17 23:10:40 +0900

    AtCoder_Beginner_Contest_269

---
+++ AtCoder_Beginner_Contest_269/Submask.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var fastio *FastIo
+
+type FastIo struct {
+	Scanner *bufio.Scanner
+	Writer  *bufio.Writer
+}
+
+func NewFastIo(fp io.Reader, wfp io.Writer) *FastIo {
+	const BufSize = 2000005
+	scanner := bufio.NewScanner(fp)
+	scanner.Split(bufio.ScanWords)
+	scanner.Buffer(make([]byte, BufSize), BufSize)
+	return &FastIo{Scanner: scanner, Writer: bufio.NewWriter(wfp)}
+}
+func (i *FastIo) Text() string {
+	if !i.Scanner.Scan() {
+		panic("scan failed")
+	}
+	return i.Scanner.Text()
+}
+func (i *FastIo) Atoi(s string) int                 { x, _ := strconv.Atoi(s); return x }
+func (i *FastIo) GetNextInt() int                   { return i.Atoi(i.Text()) }
+func (i *FastIo) Atoi64(s string) int64             { x, _ := strconv.ParseInt(s, 10, 64); return x }
+func (i *FastIo) GetNextInt64() int64               { return i.Atoi64(i.Text()) }
+func (i *FastIo) Atof64(s string) float64           { x, _ := strconv.ParseFloat(s, 64); return x }
+func (i *FastIo) GetNextFloat64() float64           { return i.Atof64(i.Text()) }
+func (i *FastIo) Print(x ...interface{})            { fmt.Fprint(i.Writer, x...) }
+func (i *FastIo) Printf(s string, x ...interface{}) { fmt.Fprintf(i.Writer, s, x...) }
+func (i *FastIo) Println(x ...interface{})          { fmt.Fprintln(i.Writer, x...) }
+
+func max(x int, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func mod(m int, n int) int {
+	if m < 0 {
+		return m%n + n
+	}
+	return m % n
+}
+
+// 配列の中で引数のint型の値と同じ数を持つ要素のインデックスが帰ってくる．
+// 見つからなかった場合は配列長が帰ってくる
+func binarySearch(array []int, target int) int {
+	n := len(array)
+	i := sort.Search(n, func(i int) bool {
+		return array[i] >= target
+	})
+	return i
+}
+func main() {
+	fp := os.Stdin
+	wfp := os.Stdout
+	fastio = NewFastIo(fp, wfp)
+	defer func() {
+		fastio.Writer.Flush()
+	}()
+	solve()
+}
+func solve() {
+	n := fastio.GetNextInt64()
+	var a []int64
+	for i := 0; i <= 64; i++ {
+		if n&(1<<i) != 0 {
+			a = append(a, 1<<i)
+		}
+	}
+	for bits := 0; bits < (1 << uint64(len(a))); bits++ {
+		// bitsの各要素についてのループ
+		var index []int
+		for i := 0; i < len(a); i++ {
+			// bitsのi個目の要素の状態がonかどうかチェック
+			if (bits>>uint64(i))&1 == 1 {
+				index = append(index, i)
+			}
+		}
+		// fastio.Println(index)
+		sum := int64(0)
+		for _, v := range index {
+			sum = sum + a[v]
+		}
+		fastio.Println(sum)
+	}
+
+	//fastio.Println(a)
+}
